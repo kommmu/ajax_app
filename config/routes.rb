edbe62115978d4f145d93b7bbd55b3dc0d4864c2
kommmu <73157000+kommmu@users.noreply.github.com>
2020-11-05 16:49:17 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@
 #  get 'posts', to: 'posts#index'
 #end
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
+  root to: 'posts#index'
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
 end
